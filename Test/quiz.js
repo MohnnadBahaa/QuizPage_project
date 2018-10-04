71be7a968f68408812516e4293cf954205fcaa9d
MohnnadBahaa <mohbah026@gmail.com>
2018-10-04 14:07:12 +0300

    last version

--- Test/quiz.js
+++ Test/quiz.js
@@ -1,8 +1,9 @@
+//We search for the button, by id, that will satrt the test,and when we click it, the test will begin.
 $('#startbtn').click(function(){
-	$('#body').fadeIn(500);
-})
+	$('#body').fadeIn(1000);
+});
 
-
+//Using each to be DRY
 function each(coll, func) { 
   if (Array.isArray(coll)) { 
     for (var i = 0; i < coll.length; i++) { 
@@ -15,37 +16,39 @@ function each(coll, func) {
   } 
 }
 
-var answers;
-var counter = 0;
+// History for all completed IQ tests
+var resultsHistory = [];
+
+//A function that take the results of the test, and check them using each, and then return the number of the correct values.
+//Then the function will restore the result in addition to the name and the age of the candidate inside the resultsHistory.
+//Then the function will show the result of that candidate.
 function sub() {
-	var qus1= document.quiz.q1.value;
-	console.log(qus1);
-	var qus2= document.quiz.q2.value;
-	console.log(qus2);
-	var qus3= document.quiz.q3.value;
-	console.log(qus3);
-	var qus4= document.quiz.q4.value;
-	console.log(qus4);
-	var qus5= document.quiz.q5.value;
-	console.log(qus5);
-	var qus6= document.quiz.q6.value;
-	console.log(qus6);
-	var qus7= document.quiz.q7.value;
-	console.log(qus7);
-	var qus8= document.quiz.q8.value;
-	console.log(qus8);
-	var qus9= document.quiz.q9.value;
-	console.log(qus9);
-	var qus10= document.quiz.q10.value;
-	console.log(qus10);
-	var correctAnswers = ['holand', 'q2', 'q3', 'q4', 'q5', 'q6', 'q7', 'q8', 'q9' ,'q10'];
-	answers = [qus1, qus2, qus3, qus4, qus5, qus6, qus7, qus8, qus9, qus10]
-	for (var i = 0; i < answers.length; i++) {
-		if (answers[i] === correctAnswers[i]) {
+  var counter = 0;
+	var qus1 = document.quiz.q1.value;
+	var qus2 = document.quiz.q2.value;
+	var qus3 = document.quiz.q3.value;
+	var qus4 = document.quiz.q4.value;
+	var qus5 = document.quiz.q5.value;
+	var qus6 = document.quiz.q6.value;
+	var qus7 = document.quiz.q7.value;
+	var qus8 = document.quiz.q8.value;
+	var qus9 = document.quiz.q9.value;
+	var qus10 = document.quiz.q10.value;
+
+  var correctAnswers = ['33 percent', 'Apple', '0', '4', 'Bill is taller than Peter', 'Anxious', 'Spoke', '12', '31311313' ,'16'];
+	var answers = [qus1, qus2, qus3, qus4, qus5, qus6, qus7, qus8, qus9, qus10];
+
+	each(answers, function(value, key) {
+		if (value === correctAnswers[key]) {
 			counter++;
 		}
-	}
-	$('#result').append('<p>Your result is: ' + counter + '</p>');
+	});
+	var person = {};
+	person.personName = $('#Name').val();
+	person.age = $('#Age').val();
+	person.mark = counter;
+	resultsHistory.push(person);
+	$('#result').html('<p>Your result is: ' + counter + '</p>');
 }
 
 
